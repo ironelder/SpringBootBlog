816e2265193ef240ec65b2510666a01cdf522c29
ironelder <ironelder@geninetworks.com>
2016-08-26 17:25:33 +0900

    webjar commit

--- src/main/java/com/ironelder/blog/HelloController.java
+++ src/main/java/com/ironelder/blog/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index(Model model){
         model.addAttribute("name", "SpringBlog form ironelder");
-        return "hellojsp";
+        return "hello";
     }
 }
