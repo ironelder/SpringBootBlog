6cd79cc9110f59b8985b8d859bb9f82a3fa365af
ironelder <ironelder@geninetworks.com>
2016-08-29 13:24:48 +0900

    작업본 커밋

--- src/main/java/com/ironelder/blog/HelloController.java
+++ src/main/java/com/ironelder/blog/HelloController.java
@@ -10,8 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     @RequestMapping("/hello")
-    public String index(Model model){
+     public String index(Model model){
         model.addAttribute("name", "SpringBlog form ironelder");
         return "hello";
+    }
+
+    @RequestMapping("/blog")
+    public String blog(Model model){
+        return "blog";
+    }
+
+    @RequestMapping("/post")
+    public String post(Model model){
+        return "post";
     }
 }
